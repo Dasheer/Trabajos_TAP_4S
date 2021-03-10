f5905be15453beb807be15a91738d95fe6bfe77c
Daniel <danieleduardo.pc@outlook.com>
2021-03-09 22:43:59 -0600

    Implementación de SUMA de los números ingresados. Ejercicio 3

--- ejercicios/ejercicio2/VentanaCListener.java
+++ ejercicios/ejercicio2/VentanaCListener.java
@@ -26,6 +26,17 @@ public class VentanaCListener extends Frame implements ActionListener {
      * Inicializamos los widgets y los metodos al contenedor
      */
 
+    public static boolean isNumber(String nVal) {
+        try {
+            if (nVal != null) {
+                Integer.parseInt(nVal);
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
     public void init() {
         isClicked = false;
         btn_click = new Button("Aceptar");
@@ -62,7 +73,16 @@ public class VentanaCListener extends Frame implements ActionListener {
         if (arg0.getSource() == btn_click) {
             String txt = txt_in.getText();
             txt_result.append(txt + "\n");
+            txt_in.setText("");
+            if (isNumber(txt)) {
+                int nbr = Integer.parseInt(txt);
+                n += nbr;
+            }
 
+            if (txt.equals("SUMAR")) {
+                String suma = n + "";
+                txt_result.append("SUMA: " + suma + "\n");
+            }
         }
 
         if (arg0.getSource() == btn_clear) {
@@ -82,5 +102,5 @@ public class VentanaCListener extends Frame implements ActionListener {
     Label lbl_msg;
     TextArea txt_result;
     boolean isClicked;
-
+    int n;
 }
--- ejercicios/readme.md
+++ ejercicios/readme.md
@@ -6,4 +6,6 @@ Crear una ventana que se pueda cerrar utilizando Java y awt.
 
 Manejo de eventos a través de listeners. Utilizar ActionListener para el control de botón
 
-           
+# Ejercicio 3
+
+Modifica la clase del Ejercicio 2 e implementa una sumadora, que permita ir introduciendo los números en el cuadro de texto de abajo y cuando se proporcione la palabra SUMAR, calcule la suma de todos los números que se hayan proporcionado y se encuentren en el Área de Texto. Si existen palabras en dicha área los debe ignorar  
