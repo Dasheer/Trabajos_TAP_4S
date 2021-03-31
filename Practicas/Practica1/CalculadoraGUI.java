a66eb57f37bbd3adb370f621c2820d3416b139c3
Daniel <danieleduardo.pc@outlook.com>
2021-03-30 22:35:03 -0600

    Creación de la clase Calculadora

---
+++ Practicas/Practica1/CalculadoraGUI.java
@@ -0,0 +1,53 @@
+package Practicas.Practica1.Calculadora.java;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class CalculadoraGUI extends Frame implements ActionListener {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public Calculadora(){
+        initComponents();
+    }
+
+    public void initComponents() {
+        //
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    // Creación de los widgets
+    private Panel topPanel;
+    private Panel bottomPanel;
+
+    private Button numberOne;
+    private Button numberTwo;
+    private Button numberThree;
+    private Button numbreFour;
+    private Button numberFive;
+    private Button numberSix;
+    private Button numberSeven;
+    private Button numberEight;
+    private Button numberNine;
+    private Button numberZero;
+
+    private Button buttonPoint;
+    private Button buttonEqual;
+    private Button buttonPercentage;
+    private Button buttonAllClear;
+    private Button buttonLeftParenthesis;
+    private Button buttonRightParenthesis;
+    private Button buttonDivision;
+    private Button buttonMultiplication;
+    private Button buttonAdd;
+    private Button buttonSubtract;
+
+}
---
+++ Practicas/readme.md
@@ -0,0 +1,9 @@
+# Práctica 1 - Eventos de botónCalculadora básica
+
+Se requiere realizar una aplicación de escritorio que realice los cálculos simples de números enteros de hasta 20 dígitos. En ella solamente se podrá introducir números uno por vez seguido de una operación matemática; las operaciones matemáticas tienen prioridades para realizarse que deben aplicarse en la calculadora; el resultado final deberá mostrarse cuando se presione el operador =. Los números deberán ser introducidos en una interfaz gráfica con botones, uno por cada número disponible y uno para cada operación a realizar.
+
+Instrucciones:
+1.Revisar la información sobre eventos en los apartados de pestaña
+2.Construir el programa solucionando el problema planteado anteriormente
+3.Capturar el proceso de creación en imágenes y crear un video de los resultados
+4.Escribir un documento de resultados
