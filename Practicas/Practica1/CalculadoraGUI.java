e65fc7aa0ab3abc0f5c6777e8a91ea46f3260291
Daniel <danieleduardo.pc@outlook.com>
2021-04-01 16:53:50 -0600

    Implemetación del Método addDigit y acciones de botones

--- Practicas/Practica1/CalculadoraGUI.java
+++ Practicas/Practica1/CalculadoraGUI.java
@@ -37,6 +37,29 @@ public class CalculadoraGUI extends Frame implements ActionListener {
         this.setResizable(false);
         this.setTitle("Calculadora");
         this.setLocationRelativeTo(null);
+
+        numberOne.addActionListener(this);
+        numberTwo.addActionListener(this);
+        numberThree.addActionListener(this);
+        numberFour.addActionListener(this);
+        numberFive.addActionListener(this);
+        numberSix.addActionListener(this);
+        numberSeven.addActionListener(this);
+        numberEight.addActionListener(this);
+        numberNine.addActionListener(this);
+        numberZero.addActionListener(this);
+
+        buttonPoint.addActionListener(this);
+        buttonEqual.addActionListener(this);
+        buttonPercentage.addActionListener(this);
+        buttonAllClear.addActionListener(this);
+        buttonLeftParenthesis.addActionListener(this);
+        buttonRightParenthesis.addActionListener(this);
+        buttonDivision.addActionListener(this);
+        buttonMultiplication.addActionListener(this);
+        buttonAdd.addActionListener(this);
+        buttonSubtract.addActionListener(this);
+
     }
 
     // Creación del panel superior
@@ -71,7 +94,7 @@ public class CalculadoraGUI extends Frame implements ActionListener {
         numberOne = new Button("1");
         numberTwo = new Button("2");
         numberThree = new Button("3");
-        numbreFour = new Button("4");
+        numberFour = new Button("4");
         numberFive = new Button("5");
         numberSix = new Button("6");
         numberSeven = new Button("7");
@@ -98,7 +121,7 @@ public class CalculadoraGUI extends Frame implements ActionListener {
         bottomPanel.add(numberEight);
         bottomPanel.add(numberNine);
         bottomPanel.add(buttonDivision);
-        bottomPanel.add(numbreFour);
+        bottomPanel.add(numberFour);
         bottomPanel.add(numberFive);
         bottomPanel.add(numberSix);
         bottomPanel.add(buttonMultiplication);
@@ -130,9 +153,103 @@ public class CalculadoraGUI extends Frame implements ActionListener {
         return buttonType;
     }
 
+    public void addDigit(String number) {
+        if (textOperation.getText().equals("0")) {
+            textOperation.setText("");
+        }
+        if (!textOperation.getText().equals(null)) {
+            String addNumber = textOperation.getText() + number;
+            textOperation.setText(addNumber);
+        } else {
+            textOperation.setText(number);
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+        Object buttonPress = e.getSource();
+
+        if (buttonPress == numberOne) {
+            addDigit("1");
+        }
+
+        if (buttonPress == numberTwo) {
+            addDigit("2");
+        }
+
+        if (buttonPress == numberThree) {
+            addDigit("3");
+        }
+
+        if (buttonPress == numberFour) {
+            addDigit("4");
+        }
+
+        if (buttonPress == numberFive) {
+            addDigit("5");
+        }
+
+        if (buttonPress == numberSix) {
+            addDigit("6");
+        }
+
+        if (buttonPress == numberSeven) {
+            addDigit("7");
+        }
+
+        if (buttonPress == numberEight) {
+            addDigit("8");
+        }
+
+        if (buttonPress == numberNine) {
+            addDigit("9");
+        }
+
+        if (buttonPress == numberZero) {
+            addDigit("0");
+        }
+
+        // Implementación de botones de operaciones
+        if (buttonPress == buttonPoint) {
+            addDigit(".");
+        }
+
+        // if(buttonPress == buttonEqual) {
+        // addDigit("");
+        // }
+
+        if (buttonPress == buttonPercentage) {
+            addDigit("%");
+        }
+
+        if (buttonPress == buttonAllClear) {
+            textOperation.setText("0");
+            textResult.setText("0");
+        }
+
+        if (buttonPress == buttonLeftParenthesis) {
+            addDigit("(");
+        }
+
+        if (buttonPress == buttonRightParenthesis) {
+            addDigit(")");
+        }
+
+        if (buttonPress == buttonDivision) {
+            addDigit("/");
+        }
+
+        if (buttonPress == buttonMultiplication) {
+            addDigit("*");
+        }
+
+        if (buttonPress == buttonAdd) {
+            addDigit("+");
+        }
+
+        if (buttonPress == buttonSubtract) {
+            addDigit("-");
+        }
 
     }
 
@@ -150,7 +267,7 @@ public class CalculadoraGUI extends Frame implements ActionListener {
     private Button numberOne;
     private Button numberTwo;
     private Button numberThree;
-    private Button numbreFour;
+    private Button numberFour;
     private Button numberFive;
     private Button numberSix;
     private Button numberSeven;
