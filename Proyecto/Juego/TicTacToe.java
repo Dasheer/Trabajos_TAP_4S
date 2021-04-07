32aa95fcdab31563d0ca26d6855d421773ac90c6
Daniel <danieleduardo.pc@outlook.com>
2021-04-07 17:27:05 -0500

    Métodos menu y game, implementación de botones y menubar

--- Proyecto/Juego/TicTacToe.java
+++ Proyecto/Juego/TicTacToe.java
@@ -10,6 +10,8 @@ import javax.swing.event.*;
 public class TicTacToe extends JFrame implements ActionListener {
 
     public TicTacToe() {
+        menu();
+        game();
         initComponents();
     }
 
@@ -19,6 +21,41 @@ public class TicTacToe extends JFrame implements ActionListener {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+        this.setLayout(new BorderLayout());
+        this.add(buttons);
+    }
+
+    public void menu() {
+        menuGame = new JMenuBar();
+        main = new JMenu("Juego");
+        rebootG = new JMenuItem("Reiniciar");
+        exitG = new JMenuItem("Salir");
+
+        this.setJMenuBar(menuGame);
+        menuGame.add(main);
+        main.add(rebootG);
+        main.addSeparator();
+        main.add(exitG);
+
+    }
+
+    public void game() {
+        buttons = new JPanel();
+        buttons.setLayout(new GridLayout(3, 3, 8, 8));
+        createButton(buttonUpperLeft, "");
+        createButton(buttonTopUp, "");
+        createButton(buttonUpperRight, "");
+        createButton(buttonLeft, "");
+        createButton(buttonCenter, "");
+        createButton(buttonRight, "");
+        createButton(buttonLowerLeft, "");
+        createButton(buttonLowerDown, "");
+        createButton(buttonLowerRight, "");
+    }
+
+    private void createButton(JButton b, String s) {
+        b = new JButton(s);
+        buttons.add(b);
     }
 
     @Override
@@ -31,4 +68,19 @@ public class TicTacToe extends JFrame implements ActionListener {
         new TicTacToe();
     }
 
+    private JMenuBar menuGame;
+    private JMenu main;
+    private JMenuItem rebootG;
+    private JMenuItem exitG;
+
+    private JPanel buttons;
+    private JButton buttonUpperLeft;
+    private JButton buttonTopUp;
+    private JButton buttonUpperRight;
+    private JButton buttonLeft;
+    private JButton buttonCenter;
+    private JButton buttonRight;
+    private JButton buttonLowerLeft;
+    private JButton buttonLowerDown;
+    private JButton buttonLowerRight;
 }
